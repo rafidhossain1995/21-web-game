03d2a4cc773e2e0b8578280483cda6b9a2b4aba6
rafidhossain1995 <rafidhossain@pursuit.org>
2019-12-14 17:57:15 -0500

    Create values

---
+++ blackJack.js
@@ -0,0 +1,85 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    let deck_id
+    let start = document.querySelector(".start")
+    let stay = document.querySelector(".stay")
+    let hit = document.querySelector(".hit")
+    const cardValue = (card) =>{ 
+        let scoreBoard = document.querySelector(".scoreBoard")
+        if(card === "KING" || card === "QUEEN" || card === "JACK"){
+            score += 10
+        }else if(card ==="ACE" && value + 11 > 21){
+            score +=1
+        } else if (card === "ACE" && value + 11 < 21){
+            score += 11
+        } else {
+            score += Number(card)
+        }
+        scoreBoard.innerText = "Your score is: " + (card)
+    }
+    const shuffle = async() =>{
+        try{
+            let start = document.querySelector(".start")
+            let res = await axios.get("https://deckofcardsapi.com/api/deck/new/") 
+            deck_id = res.data.deck_id;
+            let shuffled = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/shuffle/`)
+        } catch(err){  
+            console.log(err)
+        }
+    }
+    const drawTwo = async(id) =>{
+        try{
+            let drawCards =  await axios.get(`https://deckofcardsapi.com/api/deck/${id}/draw/?count=2`)
+            let deck = document.querySelector(".blackJack")
+            //deck.innerHTML = ""
+            for(let i = 0; i < drawCards.data.cards.length; i++){
+                console.log(drawCards.data["cards"][i]["value"])
+                //debugger
+                let img = document.createElement("img")
+                let src = drawCards.data["cards"][i]["image"]
+                img.src = src
+                deck.appendChild(img)
+                deck.appendChild(stay)
+                deck.appendChild(hit)
+                cardValue(drawCards.data["cards"][i]["value"])
+            }
+
+        }catch(err){
+            console.log(err)
+            debugger
+            
+        }
+    }
+    
+    const addCard = async(id) =>{
+        try{
+            let drawCards = await axios.get(`https://deckofcardsapi.com/api/deck/new/draw/?count=1`)
+            let deck2 = document.querySelector(".blackJack")
+            //deck2.innerHTML = ""
+            for(let i = 0; i < drawCards.data.cards.length; i++){
+                console.log(drawCards.data["cards"][i]["value"])
+                let img2 = document.createElement("img")
+                let src2 = drawCards.data["cards"][i]["image"]
+                img2.src = src2
+                deck2.appendChild(img2)
+                deck2.appendChild(stay)
+                deck2.appendChild(hit)
+                cardValue(drawCards.data["cards"][i]["value"])
+                
+                
+            }
+        }catch(err){
+            console.log(err)
+        }
+    }
+    
+    let score = 0 
+    start.addEventListener("click", ()=>{
+        drawTwo(deck_id)
+        cardValue()
+    })
+    hit.addEventListener("click", ()=>{
+        addCard(deck_id)
+        cardValue()
+    })
+    shuffle()
+})
