c925d8405bfe71fdfaa73854d4cb09a731e93735
rafidhossain1995 <rafidhossain@pursuit.org>
2019-12-15 11:20:08 -0500

    Finish Dealer Hand

--- blackJack.js
+++ blackJack.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    //let deck_id
+    let deck_id
     let start = document.querySelector(".start")
     let stay = document.querySelector(".stay")
     let hit = document.querySelector(".hit")
     let score = 0 
-    const cardValue = (card) =>{ 
+    let score2 = 0
+    const cardValue = async (card) =>{ 
         let scoreBoard = document.querySelector(".scoreBoard")
         if(card === "KING" || card === "QUEEN" || card === "JACK"){
             score += 10
@@ -12,15 +13,23 @@ document.addEventListener("DOMContentLoaded", ()=>{
             score +=1
         } else if (card === "ACE" && score + 11 < 21){
             score += 11
-        } else {
-            score += card  
+        } else if (card) {
+            score += Number(card) 
         }
-        scoreBoard.innerHTML = "Your score is: " + score
-        //score.appendChild(scoreBoard)
-        //debugger
-        //scoreBoard.appendChild(card)
-        //debugger
+        scoreBoard.innerHTML = `Your score is: ${score}`
     }
+    const dealerValue = async(card2) =>{
+        let scoreBoard2 = document.querySelector(".dealerScore")
+        if(card2 === "KING" || card2 === "QUEEN" || card2 === "JACK"){
+            score2 +=10
+        }else if (card2 === "ACE" && score2 + 11 > 21){
+            score2 += 11
+        } else if (card2){
+            score2 += Number(card2)
+        }
+        scoreBoard2.innerHTML = `Your score is: ${score2}`
+    }
+
     const shuffle = async() =>{
         try{
             let start = document.querySelector(".start")
@@ -71,14 +80,35 @@ document.addEventListener("DOMContentLoaded", ()=>{
         }catch(err){
             console.log(err)
         }
+    }
+    const dealerHand = async() =>{
+        try{
+            let drawCards = await axios.get (`https://deckofcardsapi.com/api/deck/new/draw/?count=3`)
+            let opponentDeck = document.querySelector(".dealer")
+            for(let i = 0; i < drawCards.data.cards.length; i++){
+                console.log(drawCards.data["cards"][i]["value"])
+                let img3 = document.createElement("img")
+                let src3 = drawCards.data["cards"][i]["image"]
+                img3.src = src3 
+                opponentDeck.appendChild(img3)
+                opponentDeck.appendChild(stay)
+                opponentDeck.appendChild(hit)
+                dealerValue(drawCards.data["cards"][i]["value"])
+            }
+        } catch(err){
+            console.log(err)
+        }
     }  
-    start.addEventListener("click", ()=>{
+    start.addEventListener("click",  ()=>{
         drawTwo(deck_id)
         //cardValue()
     })
-    hit.addEventListener("click", ()=>{
+    hit.addEventListener  ("click",  ()=>{
         addCard(deck_id)
         //cardValue()
+    })
+    stay.addEventListener ("click", () => {
+        dealerHand(deck_id)
     })
     
     shuffle()
